378ba1531003d3352ce2863672c6b2b9558a453a
kanangoylarov <goylerov03@gmail.com>
2024-02-25 12:40:10 +0000

    a

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char a = 'a';
+	int count = 0;
+	char letter;
 
-	for (i = 0; i < 10; i++)
+	while (count++ <= 9)
 	{
-		while (a <= 'z')
-		{
-			_putchar(a);
-			a++;
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
