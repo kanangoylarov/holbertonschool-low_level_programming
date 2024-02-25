b97cedba1bccddd417658477ff705de07c8ba8fc
kanangoylarov <goylerov03@gmail.com>
2024-02-25 08:29:22 +0000

    a

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - check the code.
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+
+	{
+	putchar(a);
+	a++;
+	}
+	putchar('\n');
+}
