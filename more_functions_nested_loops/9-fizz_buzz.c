df165cb6d2f436a3d362ba2301ed163a0696d9a8
kanangoylarov <goylerov03@gmail.com>
2024-02-27 08:28:46 +0000

    a

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Description: FizzBuzz
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 > 0) && (i % 5 > 0))
+		{
+			printf("%d ", i);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3 == 0))
+		{
+			printf("Fizz");
+		}
+		else
+			printf("Buzz");
+	}
+}
