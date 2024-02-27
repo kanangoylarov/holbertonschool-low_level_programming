bdab2b8216e94ac7f34ed35ec7b3173d233439fb
kanangoylarov <goylerov03@gmail.com>
2024-02-27 07:35:44 +0000

    a

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_line - Draws a straight line according to parameter
+  * @n: The number of lines to draw
+  *
+  * Return: empty
+  */
+void print_line(int n)
+{
+
+	int j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (; j <= n; j++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
