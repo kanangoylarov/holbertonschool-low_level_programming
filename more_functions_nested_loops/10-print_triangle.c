58e498217d57e82b813b84947659141a2d8fc93e
kanangoylarov <goylerov03@gmail.com>
2024-02-27 09:16:36 +0000

    a

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * print_square - Prints n squares according n number of times
+  * @size: The number of squares/number of times
+  *
+  * Return: empty
+  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - (i + 1); j++)
+			{
+				_putchar(32);
+			}
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
