f3115b930ecd63e56caf1935f287c783eb4ab947
kanangoylarov <goylerov03@gmail.com>
2024-02-27 06:54:48 +0000

    a

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@
 void more_numbers(void)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	for (; i <= 9; i++)
 	{
-		for (; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 		if (j > 9)
 			_putchar((j / 10) + '0');
