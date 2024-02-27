f8a8806d13b036668a50007c8b950bb25138042a
kanangoylarov <goylerov03@gmail.com>
2024-02-27 08:00:09 +0000

    a

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * print_square - Prints n squares according n number of times
+  * @size: The number of squares/number of times
+  *
+  * Return: empty
+  */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar(32);
+	}
+
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
