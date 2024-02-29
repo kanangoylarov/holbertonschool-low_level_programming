7b92af2a672e6609da792a32b6d9380dae95e8e1
kanangoylarov <goylerov03@gmail.com>
2024-02-29 16:25:10 +0000

    a

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	char *z = s;
+	int a;
+	a = 0;
+
+	while (*(z + a) != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
