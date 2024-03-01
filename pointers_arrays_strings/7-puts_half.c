2766b92a42874447b96030d4a03975767f8602a3
kanangoylarov <goylerov03@gmail.com>
2024-03-01 14:13:25 +0000

    a

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,10 @@ void puts_half(char *str)
 	int l = 0;
 	int j = 0;
 	char *z = str;
+
 	while (*(z + l) != '\0')
 		l++;
-	for (j = l / 2; j <= l; j++)
+	for (j = l / 2; j < l; j++)
 	{
 		_putchar(z[j]);
 	}
