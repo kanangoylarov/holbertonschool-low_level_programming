334599204c447d1949be2141a034501387642327
kanangoylarov <goylerov03@gmail.com>
2024-03-03 12:52:38 +0000

    a

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,10 +7,31 @@
  */
 char *cap_string(char *str)
 {
-	int a = 0;
+	int index = 0;
 
-	while (*(str + a))
+	while (str[index])
 	{
-		_putchar(str[a]);
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
+
+	return (str);
 }
