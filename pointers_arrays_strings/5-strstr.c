74360d436c688e5525de1a221e7f75f780d62dc2
kanangoylarov <goylerov03@gmail.com>
2024-03-05 14:59:28 +0000

    what is life wonder if we are always busy

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: If the substring is located - a pointer to the beginning
+ *                                       of the located substring.
+ *         If the substring is not located - NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+
+	while (*(needle + d))
+		d++;
+
+	while (*(haystack + a))
+	{
+		if (haystack[a] == needle[b])
+		{
+			c++;
+			break;
+		}
+		a++;
+	}
+	if (c == 1)
+	{
+		while (*(needle + b))
+		{
+			if (needle[b] != haystack[a + b])
+				break;
+			b++;
+		}
+	}
+	if (b == d)
+		return (haystack + a);
+
+	return ('\0');
+}
