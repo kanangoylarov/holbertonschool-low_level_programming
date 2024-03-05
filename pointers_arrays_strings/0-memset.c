935049d98de2ee565ffa7b9f0fdd3792d3fff38d
kanangoylarov <goylerov03@gmail.com>
2024-03-05 06:31:23 +0000

    sala

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,10 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a = 0;
+	unsigned int a = 0;
 
-	while (*(s + a) && a < n)
+	while (a < n)
+	{
 		s[a] = b;
 		a++;
+	}
 	return (s);
 }
