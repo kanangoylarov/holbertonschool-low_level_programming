0e5456d87fd52dfb6b36df411b2eaae1d661274c
kanangoylarov <goylerov03@gmail.com>
2024-03-08 16:44:24 +0000

    slam

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,17 +4,13 @@
  * @n: The number to find the factorial of.
  *
  * Return: If n > 0 - the factorial of n.
- *         If n < 0 - 1 to indicate an error.
  */
 int factorial(int n)
 {
-	if (n == 0)
+	if (n > 0)
+		return (n * factorial(n - 1));
+	else if (n == 0)
 		return (1);
 	else
-	{
-		if (n < 0)
-			return (-1);
-		else
-			return n * factorial(n - 1);
-	}
+		return (-1);
 }
