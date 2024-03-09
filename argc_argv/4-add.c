8be44cb95a932edb6e431cdac32a3448bf1cde17
kanangoylarov <goylerov03@gmail.com>
2024-03-09 14:11:13 +0000

    sala

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,13 +12,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int num;
-	int i;
+	int num, digit, sum = 0;
 
-	for (i = 0; i < argc; i++)
+	for (num = 1; num < argc; num++)
 	{
-		num += atoi(argv[i]);
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
 	}
-	printf("%d\n", num);
+
+	printf("%d\n", sum);
+
 	return (0);
 }
