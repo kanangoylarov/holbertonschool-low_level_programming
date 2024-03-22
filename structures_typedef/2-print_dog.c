bb739f96a9e219fe9af43f7126a458810b515f34
kanangoylarov <goylerov03@gmail.com>
2024-03-22 13:19:34 +0000

    s

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - definitiooo.
+ *
+ * @*d: is pointer to variables.
+ *
+ * Returns: Always is 0.
+ */
+void print_dog(struct dog *d)
+{
+	int i = 0;
+	if (d != 0)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age < 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner)
+	}
+}
