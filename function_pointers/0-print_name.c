d002f89919fc7224ef1ba567841beabb9df3cc2b
kanangoylarov <goylerov03@gmail.com>
2024-03-27 08:26:12 +0000

    salam

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <function_pointers.h>
+/*
+ *print_name - Is function in c.
+ *
+ *@name: Get name form main.
+ *
+ *@f: Is getting function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (z == NULL || f == NULL)
+		return;
+	char *z = name;
+	f(char *z);
+}
