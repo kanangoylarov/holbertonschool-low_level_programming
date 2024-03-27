3a8d22c1503134d80e455bd5fe9542d74faec7c3
kanangoylarov <goylerov03@gmail.com>
2024-03-27 09:14:39 +0000

    salam

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-/*
- * print_name - Is function in c.
- *
- *@name: Get name form main.
- *
- *@f: Is getting function
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
