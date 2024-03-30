fad293dae0a36ebe3eb3b660be28f10d331ee272
kanangoylarov <goylerov03@gmail.com>
2024-03-30 11:23:23 +0000

    my_commits

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - is my function.
+ * @n: is my var.
+ * @...: is elements.
+ * Return: sum of elements.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int z = 0;
+	va_list ap;
+	va_start(ap, n);
+	for (int i = 0; i <= n; i++)
+	{
+		z = z + va_arg(ap, int);
+	}
+	return (z);
+}
