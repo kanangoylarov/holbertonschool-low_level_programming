3593693645809de9abb04d796cba210410c495dc
kanangoylarov <goylerov03@gmail.com>
2024-03-30 13:39:43 +0000

    my_commits

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,8 +3,3 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-struct f_dt
-{
-        char *identifier;
-        void (*f)(char *, va_list);
-};
