d06e8f6b909f9ef50c0cdbc82606cd36cbba3b7d
kanangoylarov <goylerov03@gmail.com>
2024-03-30 11:36:45 +0000

    my_commits

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,9 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 	va_start(ap, n);
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		z = z + va_arg(ap, int);
 	}
+	va_end(ap);
 	return (z);
 }
