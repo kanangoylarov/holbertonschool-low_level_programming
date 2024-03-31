d10dcf894780a43d9098ab372a28418826a85d33
kanangoylarov <goylerov03@gmail.com>
2024-03-31 09:28:07 +0000

    my_commits

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,68 +1,51 @@
 #include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-struct f_dt {
-    char *identifier;
-    void (*f)(char *, va_list);
-};
+/**
+ * print_all - To be is a to be the value of a variable
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: printing everything
+ */
+void print_all(const char * const format, ...)
+{
+	size_t size = 1, i = 0;
+	const char *formatcp = format, *str;
+	va_list ap;
 
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
-
-void print_all(const char * const format, ...);
-
-void print_a_char(char *separator, va_list args) {
-    printf("%s%c", separator, va_arg(args, int));
+	va_start(ap, format);
+	while (format && i < size)
+	{
+		size = strlen(format);
+		switch (*(formatcp++))
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			str = va_arg(ap, char*);
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		if (i != (size - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 }
-
-void print_a_integer(char *separator, va_list args) {
-    printf("%s%d", separator, va_arg(args, int));
-}
-
-void print_a_float(char *separator, va_list args) {
-    printf("%s%f", separator, va_arg(args, double));
-}
-
-void print_a_char_ptr(char *separator, va_list args) {
-    char *arg = va_arg(args, char *);
-
-    if (arg == NULL) {
-        printf("%s%s", separator, "(nil)");
-        return;
-    }
-
-    printf("%s%s", separator, arg);
-}
-
-void print_all(const char * const format, ...) {
-    va_list args;
-    struct f_dt form_types[] = {
-        { "c", print_a_char },
-        { "i", print_a_integer },
-        { "f", print_a_float },
-        { "s", print_a_char_ptr }
-    };
-    unsigned int i = 0;
-    unsigned int j = 0;
-    char *separator = "";
-
-    va_start(args, format);
-
-    while (format != NULL && format[i]) {
-        j = 0;
-        while (j < 4) {
-            if (format[i] == *form_types[j].identifier) {
-                form_types[j].f(separator, args);
-                separator = ", ";
-            }
-            j++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    printf("\n"};
-}
-
