9e7df31bbd5d9b4c1e9d63e8eec32cb90079b4f4
kanangoylarov <goylerov03@gmail.com>
2024-04-04 12:30:13 +0000

    new

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,13 +1,14 @@
-#ifndef __FILE_NAME__
-#define __FILE_NAME__
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-void premain();
-
-typedef struct list_s *listPtr;
 
 /**
  * struct list_s - singly linked list
@@ -16,22 +17,19 @@ typedef struct list_s *listPtr;
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *              for Holberton project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-	void (*f)(void);
 } list_t;
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-
-void free_list(list_t *);
-list_t *add_node_end(list_t **, const char *);
-list_t *add_node(list_t **, const char *);
-size_t list_len(const list_t *);
-size_t print_list(const list_t *);
-listPtr lastNode(listPtr);
-
-#endif
+#endif /* LISTS_H */
